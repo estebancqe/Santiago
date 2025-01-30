c2215f77437863ff4e93eb8fd2c29df181440611
estebancqe <estebancqe@gmail.com>
2025-01-30 18:36:31 -0500

    Update local_build.sh

--- local_build.sh
+++ local_build.sh
@@ -1,3 +1,5 @@
+cd Documents/Proyectos/Santiago
+python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
@@ -7,3 +9,4 @@ reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
+
