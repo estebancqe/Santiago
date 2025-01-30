a7d630e6d8fd56efe8ceb77725aecd2cccb63aa1
estebancqe <estebancqe@gmail.com>
2025-01-30 18:30:08 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Santiago",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://santiago-desing.vercel.app"
+    ]
 )
